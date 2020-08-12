8ff0f7c864501ef91fdb50c907ef5016c6768723
Peter Bourget <pbourget@w6op.com>
2020-08-12 14:10:53 -0700

    More cleanup.

--- xCluster/Network Components/Utility.swift
+++ xCluster/Network Components/Utility.swift
@@ -15,19 +15,13 @@ extension String {
     }
 }
 
-extension Queue: CustomStringConvertible {
-    public var description: String {
-        return list.description
-    }
-}
-
 // find all items in an array that match a given predicate
 // https://learnappmaking.com/find-item-in-array-swift/#finding-all-items-in-an-array-with-allwhere
-extension Array where Element: Equatable {
-    func all(where predicate: (Element) -> Bool) -> [Element]  {
-        return self.compactMap { predicate($0) ? $0 : nil }
-    }
-}
+//extension Array where Element: Equatable {
+//    func all(where predicate: (Element) -> Bool) -> [Element]  {
+//        return self.compactMap { predicate($0) ? $0 : nil }
+//    }
+//}
 
 // round a Float to a number of digits
 // let x = Float(0.123456789).roundTo(places: 4)
@@ -36,141 +30,6 @@ extension Float {
     func roundTo(places:Int) -> Float {
         let divisor = pow(10.0, Float(places))
         return (self * divisor).rounded() / divisor
-    }
-}
-
-// https://www.raywenderlich.com/848-swift-algorithm-club-swift-queue-data-structure
-public struct Queue<T> {
-    
-    // 2
-    fileprivate var list = LinkedList<T>()
-    
-    public var isEmpty: Bool {
-        return list.isEmpty
-    }
-    
-    // 3
-    public mutating func enqueue(_ element: T) {
-        list.append(value: element)
-    }
-    
-    // 4
-    public mutating func dequeue() -> T? {
-        guard !list.isEmpty, let element = list.first else { return nil }
-        
-        _ = list.remove(node: element)
-        
-        return element.value
-    }
-    
-    // 5
-    public func peek() -> T? {
-        return list.first?.value
-    }
-}
-
-public class Node<T> {
-    var value: T
-    var next: Node<T>?
-    weak var previous: Node<T>?
-    
-    init(value: T) {
-        self.value = value
-    }
-}
-// https://www.raywenderlich.com/947-swift-algorithm-club-swift-linked-list-data-structure
-public class LinkedList<T> {
-    fileprivate var head: Node<T>?
-    private var tail: Node<T>?
-    
-    public var isEmpty: Bool {
-        return head == nil
-    }
-    
-    public var first: Node<T>? {
-        return head
-    }
-    
-    public var last: Node<T>? {
-        return tail
-    }
-    
-    /// Computed property to iterate through the linked list and return the total number of nodes
-    public var count: Int {
-        guard var node = head else {
-            return 0
-        }
-        
-        var count = 1
-        while let next = node.next {
-            node = next
-            count += 1
-        }
-        return count
-    }
-    
-    public func append(value: T) {
-        let newNode = Node(value: value)
-        if let tailNode = tail {
-            newNode.previous = tailNode
-            tailNode.next = newNode
-        } else {
-            head = newNode
-        }
-        tail = newNode
-    }
-    
-    public func nodeAt(index: Int) -> Node<T>? {
-        if index >= 0 {
-            var node = head
-            var i = index
-            while node != nil {
-                if i == 0 { return node }
-                i -= 1
-                node = node!.next
-            }
-        }
-        return nil
-    }
-    
-    public func removeAll() {
-        head = nil
-        tail = nil
-    }
-    
-    public func remove(node: Node<T>) -> T {
-        let prev = node.previous
-        let next = node.next
-        
-        if let prev = prev {
-            prev.next = next
-        } else {
-            head = next
-        }
-        next?.previous = prev
-        
-        if next == nil {
-            tail = prev
-        }
-        
-        node.previous = nil
-        node.next = nil
-        
-        return node.value
-    }
-}
-
-extension LinkedList: CustomStringConvertible {
-    public var description: String {
-        var text = "["
-        var node = head
-        
-        while node != nil {
-            text += "\(node!.value)"
-            node = node!.next
-            if node != nil { text += ", " }
-        }
-        return text + "]"
     }
 }
 
